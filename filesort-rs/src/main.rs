29c4732412f5459ae976ff9fcdd6ee0bae33bdc1
viv-codes <vhafener@outlook.com>
2022-04-20 12:48:32 -0400

    Starting work

--- filesort-rs/src/main.rs
+++ filesort-rs/src/main.rs
@@ -1,3 +1,42 @@
+use cursive::Cursive;
+use cursive::views::{Button, Dialog, DummyView, EditView,
+                     LinearLayout, SelectView};
+use cursive::traits::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut siv = cursive::default();
+
+    siv.add_layer(Dialog::text("This is a survey!\nPress <Next> when you're ready.")
+        .title("Filesort-rs v0.0.1")
+        .button("Next", show_next));
+
+
+    siv.run();
 }
+
+fn show_next(s: &mut Cursive) {
+    s.pop_layer();
+    s.add_layer(Dialog::around(EditView::new()
+            .on_submit(ok)
+            .with_name("Name")
+            .fixed_width(10))
+        .title("Filesort-rs v0.0.1")
+        .button("Ok", |s| {
+            let name =
+                s.call_on_name("name", |view: &mut EditView| {
+                    view.get_content()
+                }).unwrap();
+            ok(s, &name);
+        })
+        .button("Cancel", |s| {
+            s.pop_layer();
+        })
+        ); 
+}
+
+// fn show_answer(s: &mut Cursive, msg: &str) {
+//     s.pop_layer();
+//     s.add_layer(Dialog::text(msg)
+//         .title("Results")
+//         .button("Finish", |s| s.quit()));
+// }
