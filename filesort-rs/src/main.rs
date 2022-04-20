998acf959af5ff96ab179a5693802dcf4e7ddaf2
viv-codes <vhafener@outlook.com>
2022-04-20 16:16:20 -0400

    Messing around and finding out

--- filesort-rs/src/main.rs
+++ filesort-rs/src/main.rs
@@ -1,6 +1,6 @@
 use cursive::align::HAlign;
 use cursive::traits::*;
-use cursive::views::{Dialog, EditView, TextView, DummyView, LinearLayout};
+use cursive::views::{Dialog, EditView, TextView, LinearLayout};
 use cursive::Cursive;
 
 fn main() {
@@ -15,21 +15,21 @@ fn main() {
             LinearLayout::vertical()
                 .child(TextView::new("Filesort-rs v0.0.1").h_align(HAlign::Center))
                 .child(Dialog::new()
-                                .title("Filesort-rs v0.0.1")
-                                // Padding is (left, right, top, bottom)
-                                .padding_lrtb(1, 1, 1, 0)
-                                .title("Input folder: ")
-                                .content(
-                                    EditView::new()
-                                        // Call `show_popup` when the user presses `Enter`
-                                        .on_submit(show_popup)
-                                        // Give the `EditView` a name so we can refer to it later.
-                                        .with_name("name")
-                                        // Wrap this in a `ResizedView` with a fixed width.
-                                        // Do this _after_ `with_name` or the name will point to the
-                                        // `ResizedView` instead of `EditView`!
-                                        .fixed_width(20),
-                                )
+                    .title("Filesort-rs v0.0.1")
+                    // Padding is (left, right, top, bottom)
+                    .padding_lrtb(1, 1, 1, 0)
+                    .title("Input folder: ")
+                    .content(
+                        EditView::new()
+                            // Call `show_popup` when the user presses `Enter`
+                            .on_submit(show_popup)
+                            // Give the `EditView` a name so we can refer to it later.
+                            .with_name("name")
+                            // Wrap this in a `ResizedView` with a fixed width.
+                            // Do this _after_ `with_name` or the name will point to the
+                            // `ResizedView` instead of `EditView`!
+                            .fixed_width(20),
+                    )
                 .fixed_width(30),
                 )
         )
@@ -60,15 +60,69 @@ fn main() {
 fn show_popup(s: &mut Cursive, name: &str) {
     if name.is_empty() {
         // Try again as many times as we need!
-        s.add_layer(Dialog::info("Please enter a name!"));
-    } else {
-        let content = format!("Hello {}!", name);
+        s.add_layer(Dialog::info("Please enter an input path!"));
+    } 
+    // TODO Confirm path validity and perms
+    else {
+        let content = format!("Input path={}", name);
         // Remove the initial popup
         s.pop_layer();
         // And put a new one instead
-        s.add_layer(
-            Dialog::around(TextView::new(content))
-                .button("Quit", |s| s.quit()),
-        );
+        output_popup(s, &name);
     }
+
+}
+
+fn output_popup(s: &mut Cursive, instr:  &str) {
+    s.pop_layer();
+    s.add_layer(
+        Dialog::around(
+            LinearLayout::vertical()
+                .child(TextView::new("Filesort-rs v0.0.1").h_align(HAlign::Center))
+                .child(Dialog::new()
+                    .title("Filesort-rs v0.0.1")
+                    // Padding is (left, right, top, bottom)
+                    .padding_lrtb(1, 1, 1, 0)
+                    .title("Output folder: ")
+                    .content(
+                        EditView::new()
+                            // Call `show_popup` when the user presses `Enter`
+                            .on_submit(show_popup)
+                            // Give the `EditView` a name so we can refer to it later.
+                            .with_name("name")
+                            // Wrap this in a `ResizedView` with a fixed width.
+                            // Do this _after_ `with_name` or the name will point to the
+                            // `ResizedView` instead of `EditView`!
+                            .fixed_width(20),
+                    )
+                .fixed_width(30),
+                )
+        )
+            .button("Next", |s| {
+                // This will run the given closure, *ONLY* if a view with the
+                // correct type and the given name is found.
+                let name = s
+                    .call_on_name("name", |view: &mut EditView| {
+                        // We can return content from the closure!
+                        view.get_content()
+                    })
+                    .unwrap();
+                // Run the next step
+                print_config(s, instrin, &name)
+
+                
+            })
+            // let &str instrin = instr;
+            .button("Quit", |s| s.quit())
+            .h_align(HAlign::Center),
+    );
+    
+}
+
+fn print_config(s: &mut Cursive, instrin: &str, outstr: &str) {
+    let content = format!("Input path:{}\nOutput path:{}", instrin, outstr);
+    s.pop_layer();
+    s.add_layer(
+        Dialog::text(content),
+    );
 }
