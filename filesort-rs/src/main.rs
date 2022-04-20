1e1831c5e54b2449672776b0d8def68e6bd62df4
viv-codes <vhafener@outlook.com>
2022-04-20 13:57:31 -0400

    Fixed some formatting and simplified

--- filesort-rs/src/main.rs
+++ filesort-rs/src/main.rs
@@ -1,90 +1,58 @@
-use cursive::Cursive;
 use cursive::align::HAlign;
-use cursive::views::{Button, Dialog, DummyView, EditView,
-                     LinearLayout, SelectView, TextView};
 use cursive::traits::*;
+use cursive::views::{Dialog, EditView, TextView, DummyView, LinearLayout};
+use cursive::Cursive;
 
 fn main() {
     let mut siv = cursive::default();
 
-    let text = "Welcome to filesort!";
+    // Create a dialog with an edit text and a button.
+    // The user can either hit the <Ok> button,
+    // or press Enter on the edit text.
 
-    // We'll create a dialog with a TextView serving as a title
     siv.add_layer(
         Dialog::around(
             LinearLayout::vertical()
-                .child(TextView::new("Filesort v0.0.1").h_align(HAlign::Center))
-                // Use a DummyView as spacer
-                .child(DummyView.fixed_height(1))
-                // Disabling scrollable means the view cannot shrink.
-                
+                .child(TextView::new("Filesort-rs v0.0.1").h_align(HAlign::Center))
                 .child(Dialog::new()
-                .title("Input folder: ")
-                // Padding is (left, right, top, bottom)
-                .padding_lrtb(1, 1, 1, 0)
-                .content(
-                    EditView::new()
-                        // Call `show_popup` when the user presses `Enter`
-                        .on_submit(show_popup)
-                        // Give the `EditView` a name so we can refer to it later.
-                        .with_name("name")
-                        // Wrap this in a `ResizedView` with a fixed width.
-                        // Do this _after_ `with_name` or the name will point to the
-                        // `ResizedView` instead of `EditView`!
-                        .fixed_width(20),
+                                .title("Filesort-rs v0.0.1")
+                                // Padding is (left, right, top, bottom)
+                                .padding_lrtb(1, 1, 1, 0)
+                                .title("Input folder: ")
+                                .content(
+                                    EditView::new()
+                                        // Call `show_popup` when the user presses `Enter`
+                                        .on_submit(show_popup)
+                                        // Give the `EditView` a name so we can refer to it later.
+                                        .with_name("name")
+                                        // Wrap this in a `ResizedView` with a fixed width.
+                                        // Do this _after_ `with_name` or the name will point to the
+                                        // `ResizedView` instead of `EditView`!
+                                        .fixed_width(20),
+                                )
+                .fixed_width(30),
                 )
-                .button("Ok", |s| {
-                    // This will run the given closure, *ONLY* if a view with the
-                    // correct type and the given name is found.
-                    let name = s
-                        .call_on_name("name", |view: &mut EditView| {
-                            // We can return content from the closure!
-                            view.get_content()
-                        })
-                        .unwrap();
-    
-                    // Run the next step
-                    show_popup(s, &name);
-                }))
-
-
-                .child(Dialog::new()
-                .title("Output folder:")
-                // Padding is (left, right, top, bottom)
-                .padding_lrtb(1, 1, 1, 0)
-                .content(
-                    EditView::new()
-                        // Call `show_popup` when the user presses `Enter`
-                        .on_submit(show_popup)
-                        // Give the `EditView` a name so we can refer to it later.
-                        .with_name("name")
-                        // Wrap this in a `ResizedView` with a fixed width.
-                        // Do this _after_ `with_name` or the name will point to the
-                        // `ResizedView` instead of `EditView`!
-                        .fixed_width(20),
-                )
-                .button("Ok", |s| {
-                    // This will run the given closure, *ONLY* if a view with the
-                    // correct type and the given name is found.
-                    let name = s
-                        .call_on_name("name", |view: &mut EditView| {
-                            // We can return content from the closure!
-                            view.get_content()
-                        })
-                        .unwrap();
-    
-                    // Run the next step
-                    show_popup(s, &name);
-                }))
-                
-                .fixed_width(60),
         )
-        .button("Ok", |s| s.quit())
-        .button("Quit", |s| s.quit())
-        .h_align(HAlign::Center),
-    );
+            .button("Next", |s| {
+                // This will run the given closure, *ONLY* if a view with the
+                // correct type and the given name is found.
+                let name = s
+                    .call_on_name("name", |view: &mut EditView| {
+                        // We can return content from the closure!
+                        view.get_content()
+                    })
+                    .unwrap();
 
-siv.run();
+                // Run the next step
+                show_popup(s, &name);
+            })
+            .button("Quit", |s| s.quit())
+            .h_align(HAlign::Center),
+        );
+
+    
+
+    siv.run();
 }
 
 // This will replace the current layer with a new popup.
@@ -101,7 +69,6 @@ fn show_popup(s: &mut Cursive, name: &str) {
         s.add_layer(
             Dialog::around(TextView::new(content))
                 .button("Quit", |s| s.quit()),
-                // .button("Ok", |s| s.)
         );
     }
 }
